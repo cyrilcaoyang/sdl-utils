cefc1660cee181b111b2d61ea6a4d63b01a246e2
YangMacbook_m2 <cyrilcaoyang@gmail.com>
2025-03-26 17:21:57 -0400

    adding sqlite support plan

---
+++ src/sdl_utils/sdl_sqlite3.py
@@ -0,0 +1,9 @@
+"""
+A light-weight module that handles interaction with SDLite3
+"""
+
+# sending queries from python to sqlite3 and back
+
+# Return Pandas or Polars Objects
+
+# using PyPika which interact with different types of SQL
